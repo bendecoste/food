9bf6996f34ad8dcd9dac3056da1596d88dabbe8b
Benjamin DeCoste <bendecoste@gmail.com>
2013-05-23 12:31:39 -0300

    start multiplaying

--- static/js/food_ctrl.js
+++ static/js/food_ctrl.js
@@ -1,15 +1,43 @@
 function FoodCtrl($scope) {
+  this.platform = new goinstant.Platform();
+  this.keyPreface = 'bendecoste/foodhack';
+  this.keys = {};
+  this._$scope = $scope;
+
+  this.listen();
+
   $scope.foods = [
-    { name: 'burritos', desc: 'yum' },
-    { name: 'nachos', desc: 'yum' },
-    { name: 'fries', desc: 'yum' },
-    { name: 'whatever', desc: 'yum' },
-    { name: 'dude', desc: 'yum' },
+    // { name: 'burritos', desc: 'yum' },
+    // { name: 'nachos', desc: 'yum' },
+    // { name: 'fries', desc: 'yum' },
+    // { name: 'whatever', desc: 'yum' },
+    // { name: 'dude', desc: 'yum' },
   ];
 
   $scope.addFood = function() {
     $scope.foods.push({ name: $scope.newFood.name, desc: $scope.newFood.desc });
+    this.newFood($scope.newFood);
+
     $scope.newFood.name = '';
     $scope.newFood.desc = '';
-  }
+
+  }.bind(this);
 }
+
+FoodCtrl.prototype.newFood = function(food) {
+  this.keys.newFood.set(food);
+};
+
+FoodCtrl.prototype.listen = function() {
+  this.keys = {
+    newFood: this.platform.key(this.keyPreface + '/newFood')
+  };
+
+  this.keys.newFood.on('set', _.bind(this._handleNewFood, this));
+};
+
+FoodCtrl.prototype._handleNewFood = function(data) {
+  console.log('data', data.value.name, data.value.desc, data);
+  this._$scope.foods.push({name: data.value.name, desc: data.value.desc});
+  this._$scope.$apply();
+};
