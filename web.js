6063878fbc9161eb7d88abcd0c9a8134042208fc
Benjamin DeCoste <bendecoste@gmail.com>
2013-05-23 11:03:12 -0300

    merge

--- web.js
+++ web.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
+var path = require('path');
+
 app.use(express.logger());
 
-app.use('/', express.static(__dirname + '/static'));
+app.configure(function() {
+  app.use('/js', express.static(path.join(__dirname, 'static/js')));
+  app.use('/css', express.static(path.join(__dirname, 'static/css')));
+});
+
+app.get('/', function(request, response) {
+    response.sendfile('static/index.html');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
