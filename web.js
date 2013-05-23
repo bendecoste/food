27319be2b6af5da63788587ce095ec98edf3bd77
Benjamin DeCoste <bendecoste@gmail.com>
2013-05-23 19:43:45 -0300

    added nyancat and fartscroll

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ app.configure(function() {
   app.use('/js', express.static(path.join(__dirname, 'static/js')));
   app.use('/css', express.static(path.join(__dirname, 'static/css')));
   app.use('/images', express.static(path.join(__dirname, 'static/images')));
+  app.use('/audio', express.static(path.join(__dirname, 'static/audio')));
 });
 
 app.get('/', function(request, response) {
