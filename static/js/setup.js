e0c2a22a976d232ff840f366e1dbe52adf0c32dd
James Bowes <jbowes@repl.ca>
2013-05-23 18:00:39 -0300

    pull out search and add title
    With easter egg!

--- static/js/setup.js
+++ static/js/setup.js
@@ -4,4 +4,20 @@ app.run(function($rootScope) {
   $rootScope.prefix = 'jbowes/foodhack';
 
   window.go = $rootScope.go;
+
+  var spinning = false;
+  var titleSpin = $rootScope.go.key($rootScope.prefix + '/spin');
+  titleSpin.on('set', function() {
+    spinning = !spinning;
+    $('.icon-food').each(function(index, element) {
+      $(element).toggleClass('icon-spin');
+    });
+  });
+
+  $('#title').on('click', function() {
+    titleSpin.set(!spinning);
+    $('.icon-food').each(function(index, element) {
+      $(element).toggleClass('icon-spin');
+    });
+  });
 });
--- static/setup.js
+++ static/setup.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   window.go = new goinstant.Platform();
   window.prefix = 'jbowes/foodhack';
   console.log('go setup!');
+
+  $('#title').on('click', function() {
+    console.log('clicked');
+    $('.icon-food').each(function(index, element) {
+      this.addClass('icon-spin');
+
+    });
+  });
 });
